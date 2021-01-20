424ae74c52f487f2ac093d78826203c67eec1a5c
unknown <you@example.com>
2021-01-20 12:41:37 +0800

    update: finish email-unique

--- Ajax/server/app.js
+++ Ajax/server/app.js
@@ -51,6 +51,26 @@ app.get("/cache", (req, res) => {
     res.send(result);
   });
 });
+
+// 邮箱验证案例
+
+// 邮箱地址验证
+app.get("/verifyEmailAdress", (req, res) => {
+  // 接收客户端传递过来的邮箱地址
+  const email = req.query.email;
+  // 判断邮箱地址注册过的情况
+  if (email == "3327511395@qq.com") {
+    // 设置http状态码并对客户端做出响应
+    res
+      .status(400)
+      .send({ message: "邮箱地址已经注册过了, 请更换其他邮箱地址" });
+  } else {
+    // 邮箱地址可用的情况
+    // 对客户端做出响应
+    res.send({ message: "恭喜, 邮箱地址可用" });
+  }
+});
+
 // 监听端口
 app.listen(3000);
 // 控制台显示输出
